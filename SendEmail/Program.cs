7e09ab1f55820a5595df97bff618bf60303f0ebe
Gabriel Victorino <5by5.gsantos@voeazul.com.br>
2021-09-01 17:01:30 -0300

    send email

---
+++ SendEmail/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var mock = new SendEmailCommand();
+                mock.FullName = "Gabriel Victorino";
+                mock.EmployeeNumber = 1234;
+                mock.Email = "Email CC";
+                mock.AcceptanceDate = DateTime.Now;
+                mock.AcceptanceTerm = "Aceito os termos";
+
+                var mailMessage = new MailMessage("Seu email", "Seu email");
+                mailMessage.Subject = "Acceptance Term";
+                mailMessage.IsBodyHtml = true;
+
+                MailAddress copy = new MailAddress(mock.Email);
+                mailMessage.CC.Add(copy);
+
+                mailMessage.Body = $@"<p> Nome: {mock.FullName} </p> 
+                                      <p> Re: {mock.EmployeeNumber}</p>
+                                      <p> Data de aceite: {mock.AcceptanceDate}</p>
+                                      <p> Termo de aceite: {mock.AcceptanceTerm}</p>";
+
+
+                mailMessage.SubjectEncoding = Encoding.GetEncoding("UTF-8");
+                mailMessage.BodyEncoding = Encoding.GetEncoding("UTF-8");
+
+                SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential("Seu email", mock.Senha);
+                smtpClient.EnableSsl = true;
+
+                smtpClient.Send(mailMessage);
+
+                Console.WriteLine("Seu email foi enviado com sucesso");
+                Console.ReadLine();
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Houve um erro no envio do email");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ SendEmail/SendEmailCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    public class SendEmailCommand
+    {
+        public Guid Key { get; set; }
+        public string FullName { get; set; }
+        public int EmployeeNumber { get; set; }
+        public string Email { get; set; }
+        public DateTime AcceptanceDate { get; set; }
+        public string AcceptanceTerm { get; set; }
+        public string Version { get; set; }
+        public string DeviceModel { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
